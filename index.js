c9677f78cfa0eee61c790163f6effab1c863144e
koniro <63639913+koniro@users.noreply.github.com>
2020-07-07 22:03:35 +0600

    Добавлено для /second + добавлены подсказки к ошибкам в дефолтах

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function (request, response) {
     response.send('This is my JS calculator')
   })
 
-app.post("/first", function(request, response) {
+app.post("/first", (request, response) => {
     const { arg1, arg2, op } = request.body;
     if ( Number.isFinite(arg1) && Number.isFinite(arg2)){
         
@@ -32,39 +32,42 @@ app.post("/first", function(request, response) {
                 response.status(200).json(arg1 + ' % от ' + arg2 + ' = ' + arg1 * (arg2/100));
                 break;
             default: 
-                response.status(400).json('Ошибка'); 
+                response.status(400).json('Ошибка: неверная команда. + сложение, - вычитание, * умножение, / деление, ** возведение в степень, % взятие процента.'); 
                 break;
-            
         }
-    }else{
+    } else {
         response.status(400).json('Ошибка: введите числа');
-    }
-   
-    });
+    } 
+});
 
-app.post("/second", function(request, response){
-        const {arg,oper} = request.body;
-        
-       
+app.post("/second", (request, response) => {
+    const {arg,oper} = request.body;
+    if ( Number.isFinite(arg) ) {
+    
         switch (oper){
             case 'v':
                 response.status(200).json(Math.sqrt(arg));
-            break;
+                break;
             case 'sin':
                 response.status(200).json(Math.sin(arg));
-            break;
+                break;
             case'cos':
                 response.status(200).json(Math.cos(arg));
-            break;
+                break;
             case 'tg':
                 response.status(200).json(Math.tan(arg));
+                break;
             case 'ctg':
                 response.status(200).json(1/Math.tan(arg));
-            break;
-        default: 
-            response.status(400).json('Ошибка'); 
-            break;
-        }});
+                break;
+            
+            default:
+                response.status(400).json('Ошибка: неверная команда. v вычисление квадратного корня, sin вычисление синуса, cos вычисление косинуса, tg вычисление тангенса, ctg вычисление котангенса.'); 
+                break;
+        }
+    } else {
+        response.status(400).json('Ошибка: введите число');
+    } 
+});
 
-
-app.listen(2121);
+        app.listen(2121);
