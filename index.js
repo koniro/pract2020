8ca72f2221ef5224f14c6baa28357b4199820cc3
koniro <63639913+koniro@users.noreply.github.com>
2020-07-07 22:04:33 +0600

    Revert "Add error processing for /first route"

--- index.js
+++ index.js
@@ -3,68 +3,33 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-
-app.get('/', function (request, response) {
-    response.send('This is my JS calculator')
-  })
-
-app.post("/first", function(request, response) {
+app.post("/", function(request, response) {
     const { arg1, arg2, op } = request.body;
-    if ( Number.isFinite(arg1) && Number.isFinite(arg2)){
+    let result;
+    switch (op) {
+        case '+':
+            result = arg1 + arg2;
+            break;
+        case '-':
+            result = arg1 - arg2;
+            break;
+        case '*':
+            result = arg1 * arg2;
+            break;
+        case '/':
+            result = arg1 / arg2;
+            break;
+        case '**':
+            result = Math.pow (arg1,arg2)
+            break;
+        case 'cor':
+            result = Math.sqrt(arg1);
+            break;
         
-        switch (op) {
-            case '+':
-                response.status(200).json(arg1 + arg2);
-                break;
-            case '-':
-                response.status(200).json(arg1 - arg2);
-                break;
-            case '*':
-                response.status(200).json(arg1 * arg2);
-                break;
-            case '/':
-                response.status(200).json(arg1 / arg2);
-                break;
-            case '**':
-                response.status(200).json(Math.pow(arg1 , arg2));
-                break;
-            case '%':
-                response.status(200).json(arg1 + ' % от ' + arg2 + ' = ' + arg1 * (arg2/100));
-                break;
-            default: 
-                response.status(400).json('Ошибка'); 
-                break;
-            
-        }
-    }else{
-        response.status(400).json('Ошибка: введите числа');
+        
+
     }
-   
-    });
-
-app.post("/second", function(request, response){
-        const {arg,oper} = request.body;
-        
-       
-        switch (oper){
-            case 'v':
-                response.status(200).json(Math.sqrt(arg));
-            break;
-            case 'sin':
-                response.status(200).json(Math.sin(arg));
-            break;
-            case'cos':
-                response.status(200).json(Math.cos(arg));
-            break;
-            case 'tg':
-                response.status(200).json(Math.tan(arg));
-            case 'ctg':
-                response.status(200).json(1/Math.tan(arg));
-            break;
-        default: 
-            response.status(400).json('Ошибка'); 
-            break;
-        }});
-
+    response.status(200).json(result);
+});
 
 app.listen(2121);
